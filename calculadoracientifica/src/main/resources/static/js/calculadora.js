c125867f43bf21d1ae4d6a6e7102ddea45a266a8
Less-505 <leslyherlue@gmail.com>
2026-05-17 23:41:46 -0600

    Corregir botones

--- calculadoracientifica/src/main/resources/static/js/calculadora.js
+++ calculadoracientifica/src/main/resources/static/js/calculadora.js
@@ -1,19 +1,29 @@
- function setDigito(digito){
-        let display = document.getElementById('display');
-        display.value=display.value+digito;
-    }
+function setDigito(digito) {
+    let pantalla = document.getElementById('display');
+    pantalla.value = pantalla.value + digito;
+}
 
-    function setOperador(operador){
-        let display = document.getElementById('display')
-        display.value=display.value + operador;
-    }
+function setOperador(operador) {
+    let pantalla = document.getElementById('display');
+    pantalla.value = pantalla.value + operador;
+}
 
-    function borrar() {
-        let display = document.getElementById('display');
-        display.value='';
-    }
+function borrar() {
+    let pantalla = document.getElementById('display');
+    pantalla.value = '';
+}
 
-    function obtenerResultado(){
-        let display = document.getElementById('display')
-        display.value = eval(display.value);
-    }
+function obtenerResultado() {
+    let pantalla = document.getElementById('display');
+    pantalla.value = eval(pantalla.value);
+}
+
+function calcularRaiz() {
+    let pantalla = document.getElementById('display');
+    pantalla.value = Math.sqrt(pantalla.value);
+}
+
+function calcularLog() {
+    let pantalla = document.getElementById('display');
+    pantalla.value = Math.log10(pantalla.value);
+}
